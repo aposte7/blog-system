cf094d22f89d43677ce4ff778fd249ddfa1d6d6f
aposte7 <olman4jesus@gmail.com>
2025-08-13 09:25:27 +0300

    feat: configure image remote patterns in Next.js; set up image handling for external sources

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'your host name eg.(ddffrhykkmdmiokfi.supabase.co)',
+				port: '',
+				pathname: '/storage/v1/object/public/(BUCKET NAME)/**',
+			},
+		],
+	},
+}
 
-export default nextConfig;
+export default nextConfig
